843524433483eb70ff302add7ec647be129156ae
pranay-2806 <pranayyepuganti2806@gmail.com>
2026-01-07 02:01:52 +0530

    fix cors error-2

--- Backend/server.js
+++ Backend/server.js
@@ -20,20 +20,12 @@ const allowedOrigins = [
 
 app.use(
   cors({
-    origin: (origin, callback) => {
-      if (!origin || allowedOrigins.includes(origin)) {
-        callback(null, true);
-      } else {
-        callback(new Error("Not allowed by CORS"));
-      }
-    },
-    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization"],
-    credentials: true,
+    origin: ["http://localhost:5173",
+             "https://pranay-mern-ecomerce-mongodb.onrender.com"],
+    methods: ["GET","POST","PUT","DELETE","OPTIONS"],
+    allowedHeaders: ["Content-Type","Authorization"]
   })
 );
-
-app.use("/",cors())
 
 app.use(express.json())
 
