33a3bb78a95f8d5704df3c86de6b8a72248e04b1
pranay-2806 <pranayyepuganti2806@gmail.com>
2026-01-07 00:56:36 +0530

    fix Cors preflight

--- Backend/server.js
+++ Backend/server.js
@@ -13,16 +13,19 @@ const razorpayRoutes=require("./routes/orders")
 const app=express()
 
 
-app.use(cors({
-    origin:[
-        "http://localhost:5173",
-        "https://pranay-mern-ecommerce-mongodb.onrender.com"
-    ],
-    credentials:true,
-    method:["GET","POST","PUT","DELETE","OPTIONS"],
-    allowedHeaders:["Content-Type","Authorization"]
-}))
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://pranay-mern-ecommerce-mongodb.onrender.com"
+];
 
+app.use(cors({
+  origin: allowedOrigins,
+  methods: ["GET","POST","PUT","DELETE","OPTIONS"],
+  allowedHeaders: ["Content-Type","Authorization"],
+  credentials: true,
+}));
+
+app.options("*",cors())
 
 app.use(express.json())
 
